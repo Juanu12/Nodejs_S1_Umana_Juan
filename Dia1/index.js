ae87a3cb4b78358441b7dab784ccad0b830f4f80
Juanu12 <juanferumana@hotmail.com>
2025-08-04 08:50:25 -0500

    feat: :tada: Creation of my nodejs folder

---
+++ Dia1/index.js
@@ -0,0 +1,13 @@
+console.log("Hola mundo");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('¿Cuál es tu nombre? ', (nombre) => {
+  console.log(`Hola, ${nombre}!`);
+  rl.close();
+});
